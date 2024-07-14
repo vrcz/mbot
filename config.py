b8c8a2dfc130a547c7586ad7336001f45f61a8d2
vrcz <104476423+vrcz@users.noreply.github.com>
2024-07-14 10:32:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+# أضف معرفات القنوات المطلوبة هنا
+REQUIRED_CHANNELS = os.getenv('REQUIRED_CHANNELS').split(',')
+DEVELOPER_ID = int(os.getenv('DEVELOPER_ID'))
