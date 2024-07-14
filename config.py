daa3e3b807831334c435dcfd6128b1c063142880
vrcz <104476423+vrcz@users.noreply.github.com>
2024-07-14 11:31:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
+# قراءة القيم من متغيرات البيئة
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-# أضف معرفات القنوات المطلوبة هنا
+# قراءة القنوات المطلوبة كقائمة من متغير البيئة وتحويلها إلى قائمة باستخدام split(',')
 REQUIRED_CHANNELS = os.getenv('REQUIRED_CHANNELS').split(',')
+
+# قراءة معرف المطور
 DEVELOPER_ID = int(os.getenv('DEVELOPER_ID'))
